995a9f4488fdcdc99eb2729faaa80eb0e903871b
Stephen F. Booth <me@sbooth.org>
2024-12-03 14:48:19 -0600

    Update template parameters

--- Sources/cio/include/cstream.hpp
+++ Sources/cio/include/cstream.hpp
@@ -451,10 +451,7 @@ public:
 	/// - parameter value: A reference to receive the value.
 	/// - parameter order: The desired byte order.
 	/// - returns: `true` on success, `false` otherwise.
-	template <typename T, typename = std::enable_if_t<std::is_integral_v<T> && std::is_unsigned_v<T> && (sizeof(T) == 2 || sizeof(T) == 4 || sizeof(T) == 8)>>
-#if false
 	template <typename T, typename = std::enable_if_t<std::is_same_v<T, uint16_t> || std::is_same_v<T, uint32_t> || std::is_same_v<T, uint64_t>>>
-#endif
 	bool read_uint(T& value, byte_order order) noexcept
 	{
 		if(!read(value))
