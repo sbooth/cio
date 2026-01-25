2b61935f0dae2c0117edd782fcdd4e461cf3eb7c
Stephen Booth <me@sbooth.org>
2026-01-25 11:26:14 -0600

    General cleanup (#11)

--- Package.swift
+++ Package.swift
@@ -1,31 +1,27 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-	name: "cio",
-	products: [
-		// Products define the executables and libraries a package produces, making them visible to other packages.
-		.library(
-			name: "cio",
-			targets: [
-				"cio",
-			]),
-	],
-	targets: [
-		// Targets are the basic building blocks of a package, defining a module or a test suite.
-		// Targets can depend on other targets in this package and products from dependencies.
-		.target(
-			name: "cio"),
-		.testTarget(
-			name: "cioTests",
-			dependencies: [
-				"cio",
-			],
-			swiftSettings: [
-				.interoperabilityMode(.Cxx),
-			]),
-	],
-	cxxLanguageStandard: .cxx17
+    name: "cio",
+    products: [
+        .library(
+            name: "cio",
+            targets: [
+                "cio",
+            ]),
+    ],
+    targets: [
+        .target(
+            name: "cio"),
+        .testTarget(
+            name: "cioTests",
+            dependencies: [
+                "cio",
+            ],
+            swiftSettings: [
+                .interoperabilityMode(.Cxx),
+            ]),
+    ],
+    cxxLanguageStandard: .cxx17
 )
